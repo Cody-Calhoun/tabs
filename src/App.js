9a1865b64afc8f7c8ca017474443086755d8c044
William Calhoun <williamcalhoun@MacBook-Pro.local>
2020-07-14 22:19:28 -0700

    first message

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import Tabs from './components/Tabs';
 
 function App() {
+
+  const [tabs, setTabs] = useState([
+    {
+    title: "Tab1",
+    content: "Tab One content",
+    selected: false
+    },
+    {
+      title: "Tab2",
+      content: "Tab Two content",
+      selected: false
+    },
+    {
+      title: "Tab3",
+      content: "Tab three content",
+      selected: false
+    }
+  ])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Tabs tabs={tabs} setTabs={setTabs}/>
     </div>
   );
 }
